fcc640e6e06adccc1005da405d967ad561c4b892
XolotlP <xolotl.mp@gmail.com>
2021-02-18 14:46:06 -0600

    Add our Serverless API
    Add a create note API
    
    Refactor Code
    
    Code doesn’t work just yet but it shows you what we want to accomplish:
    - We want to make our Lambda function async, and simply return the results.
    - We want to simplify how we make calls to DynamoDB. We don’t want to
      have to create a new AWS.DynamoDB.DocumentClient().
    - We want to centrally handle any errors in our Lambda functions.
    - Finally, since all of our Lambda functions will be handling API
      endpoints, we want to handle our HTTP responses in one place.
    
    Remove template files
    
    Add a get note API
    
    Add a list of all the notes API
    
    Add an update note API
    
    Add a delete note API

---
+++ get.js
@@ -0,0 +1,20 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    const params = {
+        TableName: process.env.tableName,
+        Key: {
+            userId: "123", // The id of the author
+            noteId: event.pathParameters.id, // The id of the note from the path
+        },
+    };
+
+    const result = await dynamoDb.get(params);
+    if (!result.Item) {
+        throw new Error("Item not found.");
+    }
+
+    // Return the retrieved item
+    return result.Item;
+});
