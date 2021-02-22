aa825bf7d4917b4bdfc62858dd9e6b0a888d38f5
XolotlP <xolotl.mp@gmail.com>
2021-02-21 18:28:45 -0600

    Add CORS to serverless API

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -15,6 +15,10 @@ export default function handler(lambda) {
         return {
             statusCode,
             body: JSON.stringify(body),
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credential": true,
+            },
         };
     };
 }
